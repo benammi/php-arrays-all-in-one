1b4a283295e9ffda5c2dd4c017b4c5fcb1ca3bc6
Hamza Benammi <benammihamza@gmail.com>
2022-09-22 16:03:22 +0100

    init string to array

--- src/ArrayAIO.php
+++ src/ArrayAIO.php
@@ -4,4 +4,9 @@ namespace Hamza\ArrayAIO;
 
 class ArrayAIO
 {
+    public function stringToArray(string $str, string $separator=''):array
+    {
+        $arr = explode($str,$separator);
+        return $arr;
+    }
 }
